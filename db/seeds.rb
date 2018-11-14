6f5008b0d5cb14a59b38b94cde85e95102814fbf
Etienne Lioret <kavatine99@gmail.com>
2018-11-14 07:14:03 -0500

    Journee 26, exercice 3, sauvegarde

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  price = (rand() * 100.to_f).to_i
+  guests = [1, 2, 3, 4, 5, 6]
+  n_guests = guests.sample(1)[0]
+  Flat.create(address: Faker::Address.full_address, name: Faker::Company.name,
+    description: Faker::Company.catch_phrase,
+    price_per_night: price, number_of_guests: n_guests)
+end
